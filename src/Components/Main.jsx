435b722bfd5a55830b894aae7efe5ebd127f55bb
dg0397 <rgg0397@gmail.com>
2020-07-10 12:06:30 -0400

    adding footer component

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Editor from './Editor';
 import Screen from './Screen';
+import Footer from './Footer'
 
 function Main({textContent ,handleChange , textResult, handleClick}) {
   return (
     <div className = "container" >
       <Editor innerText = {textContent} handleChange = {handleChange} handleClick = {handleClick} />
       <Screen textResult = {textResult} handleClick = {handleClick} />
+      <Footer />
     </div>
   );
 }
